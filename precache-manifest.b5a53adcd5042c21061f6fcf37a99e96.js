e1120e72e5067e69d0246a64d6500c4c3d25daa8
Antonio Marco da Silva <antomarsi@hotmail.com>
2019-10-13 17:51:56 -0300

    Updates

---
+++ precache-manifest.b5a53adcd5042c21061f6fcf37a99e96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1ea740776ad3dfca8989b3924d674f",
+    "url": "/react-prova-sw/index.html"
+  },
+  {
+    "revision": "0d5f1b94866458517857",
+    "url": "/react-prova-sw/static/css/main.f12632ba.chunk.css"
+  },
+  {
+    "revision": "b65293a0831dfdd2bd3e",
+    "url": "/react-prova-sw/static/js/2.d19d7594.chunk.js"
+  },
+  {
+    "revision": "0d5f1b94866458517857",
+    "url": "/react-prova-sw/static/js/main.265b396f.chunk.js"
+  },
+  {
+    "revision": "470930cf23e7968063fc",
+    "url": "/react-prova-sw/static/js/runtime-main.8ea44b53.js"
+  },
+  {
+    "revision": "a8c9f78261ce524c750a7cf83a870aa9",
+    "url": "/react-prova-sw/static/media/jedi_female.a8c9f782.svg"
+  },
+  {
+    "revision": "8655a8455f3210c52a56865637009709",
+    "url": "/react-prova-sw/static/media/jedi_male.8655a845.svg"
+  },
+  {
+    "revision": "5dd0d2a88d5b5a48d425308c2e229d02",
+    "url": "/react-prova-sw/static/media/sith_female.5dd0d2a8.svg"
+  },
+  {
+    "revision": "e2b17dd4c1bb0067f3cf9269c26c27fe",
+    "url": "/react-prova-sw/static/media/sith_male.e2b17dd4.svg"
+  }
+]);
